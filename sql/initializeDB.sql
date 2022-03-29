60c6cabd44c15531b1b3683d2f7b368e9f9e3be9
Lizbeth Morales <52987614+lizmorales@users.noreply.github.com>
2022-03-29 15:25:23 -0500

    uploaded initializeDB.sql

---
+++ sql/initializeDB.sql
@@ -0,0 +1,188 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bookstore
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `department` (
+  `department_id` smallint NOT NULL,
+  `department_name` varchar(60) NOT NULL,
+  `department_number` smallint NOT NULL,
+  `university_id` smallint DEFAULT NULL,
+  PRIMARY KEY (`department_id`),
+  UNIQUE KEY `department_number_UNIQUE` (`department_number`),
+  KEY `university_id_idx` (`university_id`),
+  CONSTRAINT `university_id` FOREIGN KEY (`university_id`) REFERENCES `university` (`university_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES (1,'CS',1,1),(2,'CS',2,2),(3,'Finance',3,3),(4,'CS',4,4),(5,'CS',5,5);
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `department_employee`
+--
+
+DROP TABLE IF EXISTS `department_employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `department_employee` (
+  `department_department_id` smallint NOT NULL,
+  `employee_employee_id` smallint NOT NULL,
+  `department_employee_id` smallint NOT NULL,
+  PRIMARY KEY (`department_department_id`,`employee_employee_id`),
+  UNIQUE KEY `department_employee_id_UNIQUE` (`department_employee_id`),
+  KEY `employee_id_idx` (`employee_employee_id`),
+  CONSTRAINT `department_department_id` FOREIGN KEY (`department_department_id`) REFERENCES `department` (`department_id`),
+  CONSTRAINT `employee_employee_id` FOREIGN KEY (`employee_employee_id`) REFERENCES `employee` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department_employee`
+--
+
+LOCK TABLES `department_employee` WRITE;
+/*!40000 ALTER TABLE `department_employee` DISABLE KEYS */;
+INSERT INTO `department_employee` VALUES (1,1,400),(2,2,500),(3,3,600),(4,4,700),(5,5,800);
+/*!40000 ALTER TABLE `department_employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `employee_id` smallint NOT NULL,
+  `employee_full_name` varchar(60) NOT NULL,
+  `department_id` smallint DEFAULT NULL,
+  PRIMARY KEY (`employee_id`),
+  UNIQUE KEY `employee_full_name_UNIQUE` (`employee_full_name`),
+  KEY `department_id_idx` (`department_id`),
+  CONSTRAINT `department_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'David Khg',1),(2,'Kayb Akbau',2),(3,'Jenny Kim',3),(4,'Ann Kim',4),(5,'Sunny Miles',5);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entity1`
+--
+
+DROP TABLE IF EXISTS `entity1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entity1` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entity1`
+--
+
+LOCK TABLES `entity1` WRITE;
+/*!40000 ALTER TABLE `entity1` DISABLE KEYS */;
+INSERT INTO `entity1` VALUES ('lmoral20','05132001Liz!','lmoral20@uic.edu');
+/*!40000 ALTER TABLE `entity1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `university`
+--
+
+DROP TABLE IF EXISTS `university`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `university` (
+  `university_id` smallint NOT NULL,
+  `university_name` varchar(60) NOT NULL,
+  `university_user_id` varchar(50) NOT NULL,
+  PRIMARY KEY (`university_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `university`
+--
+
+LOCK TABLES `university` WRITE;
+/*!40000 ALTER TABLE `university` DISABLE KEYS */;
+INSERT INTO `university` VALUES (1,'UIC','aaa'),(2,'UIC','bbb'),(3,'UIC','ccc'),(4,'UIC','ddd'),(5,'UIC','eee');
+/*!40000 ALTER TABLE `university` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('liz','helloWorld!','lmoral20@uic.edu'),('lizcs480','480','lmoral20@uic.edu'),('lizmo','CS480','lmoral20@uic.edu'),('lmoral20','05132001Liz!','lmoral20@uic.edu');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-29 15:23:27

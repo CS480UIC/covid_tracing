d3e20966459559682627cee15af0a90be9e162ac
hkim484 <77253136+hkim484@users.noreply.github.com>
2022-03-28 20:46:11 -0500

    added deleteDB

---
+++ sql/deleteDB.sql
@@ -0,0 +1,10 @@
+/* SQL scripts to delete all the tables of database with all its contents */
+
+DROP DATABASE university;
+DROP DATABASE department;
+DROP DATABASE department_employee;
+DROP DATABASE employee;
+DROP DATABASE student;
+DROP DATABASE vaccine_status;
+DROP DATABASE schedule;
+

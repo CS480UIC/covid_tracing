3bec4118d737114a75a03c2ea55d567c9be45f71
Lizbeth Morales <52987614+lizmorales@users.noreply.github.com>
2022-03-31 08:38:05 -0500

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -54,4 +54,37 @@ where EXISTS (select * from department_employee where employee_employee_id = e.e
 
 -- --------------------------------------Done by Haeun Kim-------------------------------------------------------------------- --
 
+-- --------------------------------------Liz Morales-------------------------------------------------------------------------- --
+
+-- A simple query searches data in one table using logic operators in the condition, and orders the result 
+-- by a particular column.
+select * 
+from department
+where department_id > 3
+order by department_id;
+
+-- An aggregate query groups data in one table and applies an aggregate function
+select vaccine_name, COUNT(*)
+from vaccine_status
+ORDER BY vaccine_name;
+
+-- From the N queries, at least one should use a numeric function.
+
+-- From the N queries, at least one should use a string function.
+
+-- From the N queries, at least one should group rows with identical values into a set of 
+-- summary rows and filter group results (GROUP BY and HAVING clauses).
+
+-- A complex query searches data in two or more tables.
+-- From the N queries, at least one should use a join (any type of join).
+select student_full_name, department_name 
+from student
+inner join department
+on student.department_id = department.department_id;
+
+-- From the N queries, at least one should use a correlated subquery (without EXIST).
+
+-- From the N queries, at least one should use a correlated subquery that uses the EXIST clause
+
+-- --------------------------------------Done by Liz Morales-------------------------------------------------------------------- --
 

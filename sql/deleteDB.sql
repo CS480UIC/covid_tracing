f0e7e9ec09c334599904a3520a03d6fd652302aa
hkim484 <77253136+hkim484@users.noreply.github.com>
2022-03-30 21:53:43 -0500

    updated

--- sql/deleteDB.sql
+++ sql/deleteDB.sql
@@ -1,10 +1,9 @@
 /* SQL scripts to delete all the tables of database with all its contents */
 
-DROP DATABASE university;
-DROP DATABASE department;
-DROP DATABASE department_employee;
-DROP DATABASE employee;
-DROP DATABASE student;
-DROP DATABASE vaccine_status;
-DROP DATABASE schedule;
+DROP TABLE IF EXISTS university;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS department_employee;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS entity1;
 
